20975d473589fe5e656be70f2169a5c8e8c0ca86
MuzaFir <88869100+muzxfir@users.noreply.github.com>
2026-08-05 03:36:06 +0400

    Update reminderService.js

--- src/services/reminderService.js
+++ src/services/reminderService.js
@@ -1,8 +1,12 @@
 import {
   addDoc,
   collection,
-  serverTimestamp
+  getDocs,
+  orderBy,
+  query,
+  serverTimestamp,
 } from "firebase/firestore";
+
 import { auth, db } from "../firebase/firebase";
 
 export async function addReminder(reminder) {
@@ -18,4 +22,22 @@ export async function addReminder(reminder) {
       createdAt: serverTimestamp(),
     }
   );
+}
+
+export async function getReminders() {
+  const user = auth.currentUser;
+
+  if (!user) return [];
+
+  const q = query(
+    collection(db, "users", user.uid, "reminders"),
+    orderBy("createdAt", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map(doc => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
 }
