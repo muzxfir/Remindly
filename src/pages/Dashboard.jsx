c126c954dec83e2e59dce553ff455d014c8104bb
MuzaFir <88869100+muzxfir@users.noreply.github.com>
2026-08-05 02:24:00 +0400

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+export default function Dashboard() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      Welcome to Remindly Dashboard 🎉
+    </div>
+  );
+}
